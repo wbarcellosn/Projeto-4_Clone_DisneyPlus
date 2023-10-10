072d3442b8d37fb26f3822de25644aa924db550b
ESAB-Cursos <esabcursos@outlook.com>
2023-10-09 21:08:14 -0300

    Criando a Hero Section e importando a fonte Avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin')
 
 function styles() {
     return gulp.src('./src/styles/*.scss') //Arquivos fontes
@@ -7,7 +8,14 @@ function styles() {
     .pipe(gulp.dest('./dist/css')); //Pasta destino
 }
 
-exports.default = styles;
+
+function images() {
+    return gulp.src('./src/images/**/*') //Arquivos fontes
+    .pipe(imagemin()) //Minificação
+    .pipe(gulp.dest('./dist/images')); //Pasta destino
+}
+
+exports.default = gulp.parallel(styles, images);
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
