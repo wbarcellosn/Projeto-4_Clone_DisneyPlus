a8901ff914d3274da39b3e0214bbe8029f06d2a5
wbarcellosn <wbarcellosn@gmail.com>
2023-10-10 21:50:27 -0300

    Criando as sections 'Assista do seu jeito', 'Baixe filmes e séries' e o FAQ

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,13 @@
-const buttons = document.querySelectorAll('[data-tab-button]');
 
 document.addEventListener('DOMContentLoaded', () => {
-
+    
+    const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
 
     for (let i = 0; i < buttons.length; i++) {
+
         buttons[i].addEventListener('click', function (button) {
+
             const tabTarget = button.target.dataset.tabButton;
             const tab = document.querySelector(`[data-tab-id=${tabTarget}]`)
             hideTabs();
@@ -13,9 +16,24 @@ document.addEventListener('DOMContentLoaded', () => {
             button.target.classList.add('shows__tabs__button--is-active')
         })
     }
+
+    for (let i = 0; i < questions.length; i++) {
+
+        questions[i].addEventListener('click', openAndCloseAnswer)
+    }
 })
 
+function openAndCloseAnswer(elemento) {
+
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
+
 function removeClassActive() {
+    const buttons = document.querySelectorAll('[data-tab-button]');
+
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].classList.remove('shows__tabs__button--is-active')
     }
