eed7a001aab11af7b657f1adc0cb6785e2426d41
wbarcellosn <wbarcellosn@gmail.com>
2023-10-13 18:49:50 -0300

    Configurando o Uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin')
+const uglify = require('gulp-uglify')
+
+function scripts() {
+    return gulp.src('./src/scripts/*.js') //Arquivos fontes
+    .pipe(uglify()) //Minificação
+    .pipe(gulp.dest('./dist/js')); //Pasta destino
+}
 
 function styles() {
     return gulp.src('./src/styles/*.scss') //Arquivos fontes
@@ -8,14 +15,14 @@ function styles() {
     .pipe(gulp.dest('./dist/css')); //Pasta destino
 }
 
-
 function images() {
     return gulp.src('./src/images/**/*') //Arquivos fontes
     .pipe(imagemin()) //Minificação
     .pipe(gulp.dest('./dist/images')); //Pasta destino
 }
 
-exports.default = gulp.parallel(styles, images);
+exports.default = gulp.parallel(styles, images, scripts);
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/scripts/*.scss', gulp.parallel(scripts))
 }
